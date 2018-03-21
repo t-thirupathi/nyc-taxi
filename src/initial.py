3a9e04d28f66584f349765566303689dbc5f3073
Thirupathi Thangavel <thirupathi.thangavel@gmail.com>
2018-03-21 15:43:22 +0530

    perf improvements

--- src/initial.py
+++ src/initial.py
@@ -1,42 +1,21 @@
-
-# coding: utf-8
-
-# In[90]:
-
-
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from sklearn.linear_model import LogisticRegression
 
-
-# In[91]:
-
-
 train = pd.read_csv('../input/train.csv')
 test = pd.read_csv('../input/test.csv')
-train['pickup_datetime'] = train['pickup_datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-test['pickup_datetime'] = test['pickup_datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
 
+train = train.head(100000)
+test = test.head(100000)
 
-# In[92]:
+train['pickup_datetime_object'] = pd.to_datetime(train['pickup_datetime'])
+test['pickup_datetime_object'] = pd.to_datetime(test['pickup_datetime'])
 
-
-train = train.head(1000)
-test = test.head(1000)
-
-
-# In[93]:
-
-
-train['day_of_week'] = train['pickup_datetime'].apply(lambda x: x.weekday())
-test['day_of_week'] = test['pickup_datetime'].apply(lambda x: x.weekday())
-train['hour_of_day'] = train['pickup_datetime'].apply(lambda x: x.hour)
-test['hour_of_day'] = test['pickup_datetime'].apply(lambda x: x.hour)
-
-
-# In[95]:
-
+train['day_of_week'] = train['pickup_datetime_object'].apply(lambda x: x.weekday())
+test['day_of_week'] = test['pickup_datetime_object'].apply(lambda x: x.weekday())
+train['hour_of_day'] = train['pickup_datetime_object'].apply(lambda x: x.hour)
+test['hour_of_day'] = test['pickup_datetime_object'].apply(lambda x: x.hour)
 
 def day_of_week_sine(x):
     return np.sin(2 * np.pi * x / 7)
@@ -51,41 +30,24 @@ train['hour_of_day_sine'] = train['hour_of_day'].apply(hour_of_day_sine)
 test['hour_of_day_sine'] = test['hour_of_day'].apply(hour_of_day_sine)
 
 
-# In[97]:
+train['dist_long'] = train['pickup_longitude'] - train['dropoff_longitude']
+test['dist_long'] = test['pickup_longitude'] - test['dropoff_longitude']
+train['dist_lat'] = train['pickup_latitude'] - train['dropoff_latitude']
+test['dist_lat'] = test['pickup_latitude'] - test['dropoff_latitude']
 
+train['distance'] = np.sqrt(np.square(train['dist_long']) + np.square(train['dist_lat']))
+test['distance'] = np.sqrt(np.square(test['dist_long']) + np.square(test['dist_lat']))
 
-def distance(pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude):
-    return np.sqrt((dropoff_latitude - pickup_latitude) ** 2 + (dropoff_longitude - pickup_longitude) ** 2)
-
-train['distance'] = train.apply(lambda x: distance(x['pickup_latitude'], x['pickup_longitude'], x['dropoff_latitude'], x['dropoff_longitude']), axis=1)
-test['distance'] = test.apply(lambda x: distance(x['pickup_latitude'], x['pickup_longitude'], x['dropoff_latitude'], x['dropoff_longitude']), axis=1)
-
-
-# In[98]:
-
-
-train_tmp = train[['passenger_count', 'vendor_id', 'distance', 'day_of_week_sine', 'hour_of_day_sine', 'trip_duration']]
-print train_tmp.corr()
-
-
-# In[100]:
-
+#train_tmp = train[['passenger_count', 'vendor_id', 'distance', 'day_of_week_sine', 'hour_of_day_sine', 'trip_duration']]
+print train.corr()
 
 x_train = train[['passenger_count', 'vendor_id', 'distance', 'day_of_week_sine', 'hour_of_day_sine']]
 y_train = train['trip_duration']
 x_test = test[['passenger_count', 'vendor_id', 'distance', 'day_of_week_sine', 'hour_of_day_sine']]
 
-
-# In[ ]:
-
-
-model = LogisticRegression()
+model = LogisticRegression(tol=0.1, solver='saga')
 model.fit(x_train, y_train)
 
-
-# In[89]:
-
-
 test['trip_duration'] = model.predict(x_test)
-test.to_csv('submission.csv', index=False)
+test[['id', 'trip_duration']].to_csv('submission.csv', index=False)
 
